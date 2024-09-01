680f2ab7c2b0669e54b106cd1041e699db290d69
Naram lohit kumar <156330921+lohitkumar1914@users.noreply.github.com>
2024-09-01 20:59:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main():
         else:
             play_file_uploaded(uploaded_file, file_type)
 
-    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.beta_columns([1,1,1,1,1,1,1,1,1,1,1])
+    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns([1,1,1,1,1,1,1,1,1,1,1])
     
     is_success=False
 
